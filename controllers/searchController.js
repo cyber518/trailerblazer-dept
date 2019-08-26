ffb4b834618ad74a02a5ba48a013803f5019c376
Mert Kuvel <mertkuvel@gmail.com>
2019-08-26 13:23:46 +0300

    Moved url to env, deleted unnecessary helper and packages

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -9,9 +9,9 @@ module.exports.search_post = function (req, res, next) {
             Authorization: process.env.AUTH_KEY
         }
     }
-    
+
     //Make request to API to get movie results
-    axios.get('https://trailerblazer.herokuapp.com/search?movie=' + req.body.query.replace(/[^a-zA-Z ]/g, "").replace(/ /g, "+"), config)
+    axios.get(process.env.API_URL + '/search?movie=' + req.body.query.replace(/[^a-zA-Z ]/g, "").replace(/ /g, "+"), config)
         .then((response) => {
             //Render search result page
             res.render('search', {
